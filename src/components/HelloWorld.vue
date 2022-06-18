0d34e370f4c50c33efae9659b2526ec0cf2f3061
Jholman <jsmayorga55@misena.edu.co>
2022-06-17 19:18:09 -0500

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,126 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <v-container-fluid class="">
+      <v-row>
+        <v-col>
+          <h1 class="mt-4 text-center title"> Sala De imagen D' mary</h1>
+        </v-col>
+        <v-col>
+          
+        </v-col>
+        <v-col>
+          
+        </v-col>
+        <v-col>
+          <button>
+          <img class="mt-5" src="https://cdn-icons-png.flaticon.com/512/3144/3144456.png" alt="" id="i" @mouseover="showAlert()" >
+          </button>
+        </v-col>
+      </v-row>
+
+    <v-row id="i2">
+      <v-col>
+      </v-col>
+      <v-col>
+        <br><br><br>
+        <h1 class="white--text">peluqueria D' Mary</h1>
+        <p class="white--text"> ¿Qué te quieres hacer Hoy?</p>
+        <v-text-field label="¿Qué te quieres hacer Hoy?" solo dende></v-text-field>
+      </v-col>
+      <v-col>
+      </v-col>
+      <v-col>
+      </v-col>
+    </v-row>
+
+    <v-row id="b">
+    </v-row>
+
+<v-row>
+  <v-col>
+    <template>
+  <v-card
+    :loading="loading" class="mx-auto my-12" max-width="374">
+    <template slot="progress">
+      <v-progress-linear color="deep-purple" height="10" indeterminate
+      ></v-progress-linear>
+    </template>
+    <v-img
+      height="250" src="https://www.blog.cazcarra.com/wp-content/uploads/2019/04/tecnicas-de-corte-de-cabello.jpg"
+    ></v-img>
+
+    <v-card-title>Corte de cabello</v-card-title>
+
+    <v-card-text>
+      <v-row
+        align="center" class="mx-0">
+        <v-rating
+          :value="5.0" color="amber" dense half-increments readonly size="14">
+        </v-rating>
+        <div class="grey--text ms-4">
+          5.0 (413)
+        </div>
+      </v-row>
+      <div class="my-4 text-subtitle-1">
+        $ • Italian, Cafe
+      </div>
+      <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+    </v-card-text>
+    <v-divider class="mx-4"></v-divider>
+    <v-card-title>Tonight's availability</v-card-title>
+    <v-card-text>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn
+        color="deep-purple lighten-2" text @click="reserve"
+      >
+        Reserve
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+      </v-col>  
+    </v-row>
+
+    </v-container-fluid>
+    
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<script>
+  export default {
+    name: 'HelloWorld',
+    
+    data: () => ({
+      loading: false,
+      selection: 1,
+    }),
+    methods:{
+      reserve () {
+        this.loading = true
+        setTimeout(() => (this.loading = false), 2000)
+      },
+      showAlert() {
+      // Use sweetalert2
+      this.$swal('Este es tu carrito de compras');
+    },
+    }
+  }
+  
+</script>
+<style>
+
+#i{
+height:40px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+#i2{
+  height:450px;
+  width: 100vw;
+  background-image: url("https://i.ibb.co/W3RhxBL/Sin-t-tulo-2.png");
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+#b{
+  width:100vw;
+  height:100px;
+  background-image :url("https://i.ibb.co/6PfLKv5/Sin-t-tulo-1.png");
 }
 </style>
